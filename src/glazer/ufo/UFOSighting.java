87032a41dd182c0601f376bec3a50c32290334f3
unknown <student1@FB-W205-W8.academic.touro.edu>
2015-10-15 18:16:08 -0400

    UFO

---
+++ src/glazer/ufo/UFOSighting.java
@@ -0,0 +1,10 @@
+package glazer.ufo;
+
+public class UFOSighting {
+	private String sightedAt;
+	private String reportedAt;
+	private String location;
+	private String shape;
+	private String duration;
+	private String description;
+}
---
+++ src/glazer/ufo/UFOSightingList.java
@@ -0,0 +1,7 @@
+package glazer.ufo;
+
+import java.util.ArrayList;
+
+public class UFOSightingList extends ArrayList<UFOSighting> {
+
+}
